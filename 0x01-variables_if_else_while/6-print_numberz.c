29557c3f1de1f15bbfd55a749fd51af0f007dfb1
Wandile Mawelela <wandile1997@gmail.com>
2023-05-26 15:53:49 +0200

    [ADD] Completed task 6: Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
