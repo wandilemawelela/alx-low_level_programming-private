b674a3f71318d81b242303cb5df8ed25d89121de
Wandile Mawelela <wandile1997@gmail.com>
2023-05-26 12:54:12 +0200

    [ADD] Completed all 0x00 tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ *
+ */
+
+int main(void)
+{
+	char* s;
+	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, s, strlen(s));
+	return (1);
+}
