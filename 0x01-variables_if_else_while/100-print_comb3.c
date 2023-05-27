597d7fd77a9a4fb5357d2b63af7093b428871c68
Wandile Mawelela <wandile1997@gmail.com>
2023-05-27 10:20:38 +0200

    [ADD] Completed task 100: Write a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			if (num1 < num2)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+
+				if (num1 != 8 || (num1 == 8 && num2 != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
