a77f67f41dc9746141c7bee7cb365621fd2c37d3
Wandile Mawelela <wandile1997@gmail.com>
2023-06-19 20:34:49 +0200

    [ADD] Completed task 8 Write a function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array -  prints n elements of an array of integers, followed by a
+ * new line.
+ * @a: Pointer to the first element of the interger array
+ * @n: Number of elements of the array to be printed
+ *
+ * Return: Nothing.
+ *
+ */
+void print_array(int *a, int n)
+{
+    int i, k, digit, num, sign;
+
+    for (i = 0; i < n; i++) {
+        num = a[i];
+        digit = 1;
+        sign = 1;
+
+        /* Determine the number of digits in the integer value */
+        if (num < 0) {
+            sign = -1;
+            num = -num;
+        }
+        while (num / digit >= 10) {
+            digit *= 10;
+        }
+
+        /* Print the sign and each digit of the integer value */
+        if (sign == -1) {
+            putchar('-');
+        }
+        while (digit > 0) {
+            k = num / digit;
+            putchar(k + '0');
+            num %= digit;
+            digit /= 10;
+        }
+
+        /* Print a comma and space after each element, except the last one */
+        if (i < n - 1) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    /* Print a new line character to terminate the line */
+    putchar('\n');
+}
