a216e09e5b00db0e343828d40ed13e6be247de63
Wandile Mawelela <wandile1997@gmail.com>
2023-08-14 14:49:17 +0200

    [ADD] Completed task 4: Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+  * File: 4-sum.h
+  * Description: computes the sum of the numbers x and y.
+  * @x: First number
+  * @y: Second number
+  *
+  */
+
+#define SUM(x, y) (x + y)
+
+#endif /* SUM_H */
