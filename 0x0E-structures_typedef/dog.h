f1681ecb35cbe11e2d1794325c76eec82d679792
Wandile Mawelela <wandile1997@gmail.com>
2023-08-14 21:08:17 +0200

    [ADD] Completed task 0: Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Basic dog struct with few members
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Dog's owner
+ *
+ * Description:  a new type struct dog with the three members
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
