aeaac1417251fb20ecb507ed6dd7ca9a9ab21f31
Wandile Mawelela <wandile1997@gmail.com>
2023-08-14 14:45:00 +0200

    [ADD] Completed task 3: Write a function-like macro ABS(x) that computes the absolute value of a number x.

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+  * File: 3-function_like_macro.h
+  * Description: function-like macro ABS(x) that computes
+  * the absolute value of a number x.
+  *
+  * @x: Number to get ABS value of.
+  *
+  */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
