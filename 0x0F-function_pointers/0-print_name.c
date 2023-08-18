abf3872e216667bfd8e5d8922fe1bd40f1bbeb21
Wandile Mawelela <wandile1997@gmail.com>
2023-08-18 13:31:52 +0200

    [ADD] Completed task 0: Write a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: Name
+ * @f: Name function
+ *
+ * Return: Nothing
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
