1f0211f0e0d9f597db2fd6aaf675521a3e784426
Wandile Mawelela <wandile1997@gmail.com>
2023-08-28 04:32:04 +0200

    [ADD] Completed task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * before_main - prints You're beat! and yet, you must allow,\nI bore
+ * my house upon my back!\n before the main function is executed.
+ *
+ * Return: Nothing.
+ *
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("(A tortoise, having a pretty good sense of a hare's nature, "
+			"challenges one to a race.)\n");
+	return (0);
+}
